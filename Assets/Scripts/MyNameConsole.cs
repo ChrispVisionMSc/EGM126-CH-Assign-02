eebc22ffddf92414520ceba7edfd52362b7a3c58
Chris_H <cd.hardwick@gmail.com>
2021-11-15 21:39:30 +0000

    Addition of MyNameConsole script

---
+++ Assets/Scripts/MyNameConsole.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNameConsole : MonoBehaviour
+{
+    public string Name = "MyName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("503403");
+        Debug.Log(Name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
